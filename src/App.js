065b6da722629f044a3dd3635d289d6f84d9e071
Mary Warrick <mewarrick@gmail.com>
2018-08-21 11:16:22 -0500

    added info page and router

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import './App.css'
 import About from './about'
 import Projects from './projects'
 import MyInfo from './my-info'
+import {withRouter, Route, Switch, Redirect, BrowserRouter as Router, Link} from 'react-router-dom'
+
 
 
 
 class App extends Component {
-
+  constructor(){
+    super()
+    this.state = {
+      selected: 'welcome'
+    }
+  }
   render() {
     return (
-      <div className='App' >
-            <MyInfo/>
-          <div className='appContainer'>
-            <About />
-            {/* <Projects /> */}
-          </div>
-          </div>);
+      <Router>
+        <div className='App' >
+          <Switch>
+            <Route exact path='/' component={MyInfo} />
+            <Route path='/about-mary' component={About} />
+            <Route path='/my-projects' component={Projects} />
+          </Switch>
+        </div>
+        </Router>);
     }
 }
 
--- src/my-info.js
+++ src/my-info.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import lorumIpsum from './images/lorum-ipsum3.svg'
+import lorumIpsum from './images/lorum-ipsum.svg'
+import {Link} from 'react-router-dom'
 
 
 
@@ -21,21 +22,21 @@ class MyInfo extends Component {
             <div className='card' >
                 <h1 className='myName'> Mary Warrick</h1>
                 <div className='logosAndLinks'>
-
+                <Link to='/about-mary'><i class="fas fa-info-circle"></i></Link>
+                <i class="fas fa-code"></i>
                <a href="https://www.linkedin.com/in/mary-warrick-1a21a7b4/"> <i class="fab fa-linkedin" ></i></a>
                 <a href="https://github.com/maubertw"> <i class="fab fa-github" ></i></a>
-                <div className='tooltip'><span className='tooltiptext'>copy mary's email to clipboard</span>
-                <i class="fas fa-envelope" onClick={(e) => this.copyEmail(e)} ></i>
-
+                <div className='tooltip'><span className='tooltiptext'>click to copy Mary's email to your clipboard</span>
+                <i class="fas fa-envelope" onClick={(e) => this.copyEmail(e)} ></i></div>
 
                 </div>
-                </div>
-
                 <input type="text" value="meaubertw@gmail.com" id="email"></input>
-                <p className='regularTextBlack'>full stack software developer,<br/>
+                <p className='regularTextBlack'><strong>Full-Stack Software Developer,</strong><br/>
                 typography enthusiast,<br/>
                 oyster shucker,<br/>
-                bonvivant.</p>
+                bonvivant.
+                </p>
+
             </div>
           </div>);
     }
